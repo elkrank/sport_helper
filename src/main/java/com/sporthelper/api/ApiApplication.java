49b46d299b47825a42a538975947cd9a39c9b9d6
elkrank <r.letailleur@gmail.com>
2023-04-12 21:58:49 +0200

    first commit

---
+++ src/main/java/com/sporthelper/api/ApiApplication.java
@@ -0,0 +1,14 @@
+package com.sporthelper.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+//start
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
